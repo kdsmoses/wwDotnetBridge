2f407846270fabd23d8e178d5d21136f73e1612c
Edward Brey <ebrey@watertight.com>
2025-11-05 09:18:20 -0600

    Add synchronization context.
    * Supports posting methods to the synchronization context to be dispatched from the message loop on the main thread.
    * Events are forwarded directly to the handler, except that events from background threads or configured to post are posted to the synchronization context.
    * When a task method completes, the status is posted on the synchronization context.
    * C# async/await uses the synchronization context by default, causing continuations to run on the main thread. .NET code that can run safely on a background thread can opt to do so with `ConfigureAwait(false)`.
    
    This commit eliminates background thread calls into FoxPro and C# continuations, avoiding race conditions and data tearing, and providing a deterministic runtime environment.

--- DotnetBridge/Utilities/EventSubscriber.cs
+++ DotnetBridge/Utilities/EventSubscriber.cs
@@ -1,98 +1,64 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿#nullable enable
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Westwind.WebConnection
 {
     /// <summary>
-    /// FoxPro interop access to .NET events. Handles all events of a source object for subsequent retrieval by a FoxPro client.
+    /// Subscribes to all events for which a handler object has corresponding methods.
     /// </summary>
-    /// <remarks>For a FoxPro program to be notified of events, it should use `wwDotNetBridge.InvokeMethodAsync` to call <see cref="WaitForEvent"/>. When <see cref="WaitForEvent"/> asynchronously completes, the FoxPro program should handle the event it returns and then call <see cref="WaitForEvent"/> again to wait for the next event. The FoxPro class `EventSubscription`, which is returned by `SubscribeToEvents`, encapsulates this async wait loop.</remarks>
     public sealed class EventSubscriber : IDisposable
     {
-        private readonly object _source;
-        private readonly List<DelegateInfo> _eventHandlers = new List<DelegateInfo>();
-        private readonly ConcurrentQueue<RaisedEvent> _raisedEvents = new ConcurrentQueue<RaisedEvent>();
-        private TaskCompletionSource<RaisedEvent> _completion = new TaskCompletionSource<RaisedEvent>();
+        private readonly object _eventSource;
+        private readonly object _handler;
+        private readonly bool _post;
+        private readonly List<(EventInfo, Delegate)> _eventDelegates = [];
 
-        public EventSubscriber(object source, String prefix = "", dynamic vfp = null)
+        private static readonly MethodInfo invokeMethod = typeof(EventSubscriber).GetMethod(nameof(InvokeMethod), BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public EventSubscriber(object eventSource, object handler, string prefix, bool post, dynamic vfp)
         {
-            // Indicates that initially the client is not waiting.
-            _completion.SetResult(null);
+            _eventSource = eventSource;
+            _handler = handler;
+            _post = post;
+            var instanceExpression = Expression.Constant(this);
+            var handlerExpression = Expression.Constant(handler);
 
-            // For each event, adds a handler that calls QueueInteropEvent.
-            _source = source;
-            foreach (var ev in source.GetType().GetEvents())
+            foreach (var eventInfo in eventSource.GetType().GetEvents())
             {
-                // handler is a PRIVATE variable defined in EventSubscription.Setup().
-                Boolean hasMethod = vfp?.Eval($"PEMSTATUS(m.handler, '{prefix}{ev.Name}', 5)") ?? true;
+                string methodName = prefix + eventInfo.Name;
+                bool hasMethod = vfp.Eval($"PEMSTATUS(m.wwDotnetBridgeEventHandler, '{methodName}', 5)");
                 if (!hasMethod)
                     continue;
 
-                var eventParams = ev.EventHandlerType.GetMethod("Invoke").GetParameters().Select(p => Expression.Parameter(p.ParameterType)).ToArray();
-                var eventHandlerLambda = Expression.Lambda(ev.EventHandlerType,
-                    Expression.Call(
-                        instance: Expression.Constant(this),
-                        method: typeof(EventSubscriber).GetMethod(nameof(QueueInteropEvent), BindingFlags.NonPublic | BindingFlags.Instance),
-                        arg0: Expression.Constant(ev.Name),
-                        arg1: Expression.NewArrayInit(typeof(object), eventParams.Select(p => Expression.Convert(p, typeof(object))))),
-                    eventParams);
-                var eventHandler = eventHandlerLambda.Compile();
-                ev.AddEventHandler(source, eventHandler);
-                _eventHandlers.Add(new DelegateInfo(eventHandler, ev));
+                var eventHandlerType = eventInfo.EventHandlerType;
+                var paramExpressions = eventHandlerType.GetMethod("Invoke").GetParameters().Select(p => Expression.Parameter(p.ParameterType, p.Name)).ToArray();
+                var arguments = paramExpressions.Select(p => Expression.Convert(p, typeof(object))).ToArray();
+                var callExpression = Expression.Call(instanceExpression, invokeMethod, handlerExpression, Expression.Constant(methodName), Expression.NewArrayInit(typeof(object), arguments));
+                var eventDelegate = Expression.Lambda(eventHandlerType, callExpression, paramExpressions).Compile();
+                eventInfo.AddEventHandler(eventSource, eventDelegate);
+                _eventDelegates.Add((eventInfo, eventDelegate));
             }
-        }
-
-        class DelegateInfo
-        {
-            public DelegateInfo(Delegate handler, EventInfo eventInfo)
-            {
-                Delegate = handler;
-                EventInfo = eventInfo;
-            }
-
-            public Delegate Delegate { get; }
-            public EventInfo EventInfo { get; }
         }
 
         public void Dispose()
         {
-            foreach (var item in _eventHandlers)
-                item.EventInfo.RemoveEventHandler(_source, item.Delegate);
-            _completion.TrySetCanceled();
+            foreach ((var eventInfo, var eventDelegate) in _eventDelegates)
+                eventInfo.RemoveEventHandler(_eventSource, eventDelegate);
+            Marshal.FinalReleaseComObject(_handler);
         }
 
-        private void QueueInteropEvent(string name, object[] parameters)
+        private void InvokeMethod(object handler, string methodName, object[] arguments)
         {
-            var interopEvent = new RaisedEvent { Name = name, Params = parameters };
-            if (!_completion.TrySetResult(interopEvent))
-                _raisedEvents.Enqueue(interopEvent);
+            if (_post || Thread.CurrentThread != wwDotNetBridge._mainThread)
+                wwDotNetBridge._synchronizationContext.Post(_ => handler.GetType().InvokeMember(methodName, BindingFlags.InvokeMethod, null, handler, arguments), null);
+            else
+                handler.GetType().InvokeMember(methodName, BindingFlags.InvokeMethod, null, handler, arguments);
         }
-
-        /// <summary>
-        /// Waits until an event is raised, or returns immediately if a queued event is available.
-        /// </summary>
-        /// <returns>The next event, or null if this subscriber has been disposed.</returns>
-        public RaisedEvent WaitForEvent()
-        {
-            if (_raisedEvents.TryDequeue(out var interopEvent)) return interopEvent;
-            _completion = new TaskCompletionSource<RaisedEvent>();
-            var task = _completion.Task;
-            
-            task.Wait();
-
-            return task.IsCanceled ? null : task.Result;
-        }
-    }
-
-    public class RaisedEvent
-    {
-        public string Name { get; internal set; }
-        public object[] Params { get; internal set; }
     }
 }
